486959d43d70d512eafae8558cae1062294c2145
Leticia Cosin <leticia.cosin@nextpage.com.br>
2020-07-07 19:08:16 -0300

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by NextPage Dev 04 on 07/07/20.
+//  Copyright © 2020 Leticia Cosin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
